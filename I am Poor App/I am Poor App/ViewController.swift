2f5b9f646506a959a02fff84708632ab5fff6dc6
Daniel Bracamontes <59780347+Dedzed101@users.noreply.github.com>
2022-02-10 12:29:54 -0800

    Finish I am Rich app and add I am Poor App

---
+++ I am Poor App/I am Poor App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Poor App
+//
+//  Created by Daniel Bracamontes on 2/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
