fb51f8f77b17c809ff8c778ad34932338601c628
Daniel Bracamontes <59780347+Dedzed101@users.noreply.github.com>
2022-02-17 20:01:52 -0800

    magic 8 ball

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+var numbers  = [45, 73, 195, 53]
+var a = numbers[0] * numbers[1]
+var b = numbers[1] * numbers[2]
+var c = numbers[2] * numbers[3]
+
+var computedNumbers = [a, b, c]
+
+print(computedNumbers)
